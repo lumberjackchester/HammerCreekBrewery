18a145c6ff80763437ac502d2c081255e01ed812
craig clark <craig.clark@williams-forrest>
2014-01-20 21:13:23 -0500

    automapping

---
+++ HammerCreekBrewing/App_Start/AutoMapperConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using HammerCreekBrewing.Data.Models;
+using HammerCreekBrewing.ViewModels;
+
+namespace HammerCreekBrewing.App_Start
+{
+    public class AutoMapperConfig
+    {
+        public static void Configure()
+        {
+            Mapper.CreateMap<Beer, BeerViewModel>();
+        }
+    }
+}
